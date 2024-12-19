3fc7b2cb5180b70cb7169100f124403e6d17d2ff
PriyankaKokkera <kokkerapriyanka2006@gmail.com>
2024-12-20 01:05:22 +0530

    Delete java script/day-18 directory

--- java script/day-18/css style.js
+++
@@ -1,6 +0,0 @@
-var heading=document.getElementById("heading")
-heading.style.color="green";
-heading.style.backgroundColor="yellow";
-heading.style.letterSpacing="10px";
-heading.style.textAlign="center";
-
